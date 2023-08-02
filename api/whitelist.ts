f18b2b7eaf2bfd6bf5543e39d1041de6b87ff1c2
TheGreatAxios <sawyer@dirtroad.dev>
2023-08-02 07:58:56 -0700

    Add Edge Config

--- api/whitelist.ts
+++ api/whitelist.ts
@@ -1,7 +1,20 @@
+import { RequestContext } from "@vercel/edge";
+import { get } from '@vercel/edge-config';
+
 export const config = {
     runtime: "edge"
 }
 
-export default(req) => {
-    return new Response("Hello World");
+type Chain = "calypso-testnet" | "nebula-testnet" | "europa-testnet" | "titan-testnet" | "chaos-testnet";
+
+type RequestObj = {
+    address: string;
+    chain: Chain;
+}
+  
+export default async(request: Request, context: RequestContext) => {
+    const { address, chain }: RequestObj = await request.json();
+    const greeting = await get('greeting');
+    
+    return new Response(JSON.stringify(greeting));
 }
